b56625aedc013d19d1a3c3d35ac4bdd1753bba4b
ryleycampbell93 <ryleycampbell93@gmail.com>
2026-08-18 22:17:25 +1000

    Improve main navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,20 @@ export default function Header() {
   return (
     <header className="header">
       <div className="container nav">
-        <Link className="brand" href="/">Local<span>Loop</span></Link>
+        <Link className="brand" href="/">
+          Local<span>Loop</span>
+        </Link>
+
         <nav className="navlinks">
-          <Link href="/">Browse</Link>
-          <Link href="/messages">Messages</Link>
+          <Link href="/">Home</Link>
+          <Link href="/browse">Browse</Link>
+          <Link href="/barters">My Barters</Link>
           <Link href="/profile">Profile</Link>
-          <Link className="btn" href="/post">Post a listing</Link>
+          <Link className="btn" href="/post">
+            Post a listing
+          </Link>
         </nav>
       </div>
     </header>
   );
 }
-
