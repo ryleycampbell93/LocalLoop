f24d906bdaab39c72bcaefef65d5cd98bac6aa6d
ryleycampbell93 <ryleycampbell93@gmail.com>
2026-08-19 18:19:30 +1000

    Rename My Barters to My Deals

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,71 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="container nav">
-        <div className="brand">
-          Local<span>Loop</span>
-        </div>
+    <header
+      style={{
+        background: "#f7f6f1",
+        borderBottom: "1px solid #e5e2da",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 1100,
+          margin: "0 auto",
+          padding: "22px 16px 16px",
+        }}
+      >
+        <Link
+          href="/"
+          style={{
+            display: "inline-block",
+            color: "#111",
+            textDecoration: "none",
+            fontSize: 28,
+            fontWeight: 900,
+            letterSpacing: "-0.04em",
+            marginBottom: 18,
+          }}
+        >
+          Local<span style={{ color: "#214d3d" }}>Loop</span>
+        </Link>
 
-        <nav className="navlinks">
-          <Link href="/">Home</Link>
-          <Link href="/browse">Browse</Link>
-          <Link href="/barters">My Barters</Link>
-          <Link href="/profile">Profile</Link>
-          <Link className="btn" href="/post">
+        <nav
+          style={{
+            display: "flex",
+            gap: 8,
+            overflowX: "auto",
+            paddingBottom: 2,
+            WebkitOverflowScrolling: "touch",
+          }}
+        >
+          <Link href="/" style={navButton}>
+            Home
+          </Link>
+
+          <Link href="/browse" style={navButton}>
+            Browse
+          </Link>
+
+          <Link href="/barters" style={navButton}>
+            My Deals
+          </Link>
+
+          <Link href="/profile" style={navButton}>
+            Profile
+          </Link>
+
+          <Link
+            href="/post"
+            style={{
+              ...navButton,
+              background: "#214d3d",
+              color: "#fff",
+              borderColor: "#214d3d",
+            }}
+          >
             Post a listing
           </Link>
         </nav>
@@ -21,3 +73,20 @@ export default function Header() {
     </header>
   );
 }
+
+const navButton = {
+  flex: "0 0 auto",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: 48,
+  padding: "0 16px",
+  border: "1px solid #dedbd3",
+  borderRadius: 14,
+  background: "#fff",
+  color: "#171917",
+  textDecoration: "none",
+  fontWeight: 800,
+  fontSize: 16,
+  whiteSpace: "nowrap" as const,
+};
